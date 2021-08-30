fd38ed45db44fbce98d6c0c5616d83e8798b180d
Gaurish-Gaur <gaurishgaur3@gmail.com>
2021-08-30 11:48:01 -0700

    Backend Updates

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
