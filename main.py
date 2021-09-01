7fc13ed1a12cdaf088c9523935a297dbe80d5eac
allisonthuang <allisontrhuang@gmail.com>
2021-09-01 11:51:20 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/Fire-extinguisher.iml
    #	.idea/misc.xml

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from flask import Flask, render_template, request
 # create a Flask instance
 app = Flask(__name__)
 
+# connects default URL to render index.html
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+# runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
+
 @app.route('/greet', methods=['GET', 'POST'])
 def greet():
     # submit button has been pushed
@@ -15,19 +25,6 @@ def greet():
     # starting and empty input default
     return render_template("greet.html", name="World")
 
-# connects default URL to render index.html
-@app.route('/')
-def index():
-    return render_template("greet.html")
-
-
-# runs the application on the development server
-if __name__ == "__main__":
-    app.run(debug=True)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 @app.route('/A/')
 def A_socialscience():
