e129b6f3bd21ef71727c96f6daebe96f107eec64
Aadya Daita <aadyadaita@gmail.com>
2021-09-01 22:19:28 -0700

    Add route construct for aboutus.html

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ from flask import Flask, render_template, request
 # create a Flask instance
 app = Flask(__name__)
 
+
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if name.__len__() != 0:  # input field has content
+            return render_template("greet.html", name=name)
+        else:
+            # starting and empty input default
+            return render_template("greet.html", name="World")
+    else:
+        return render_template("greet.html")
+
+
 # connects default URL to render index.html
+#@app.route('/')
+#def index():
+#   return render_template("greet.html")
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -15,20 +35,10 @@ def aboutus():
     return render_template("aboutus.html")
 
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if name.__len__() != 0:  # input field has content
-            return render_template("greet.html", name=name)
-    # starting and empty input default
-    return render_template("greet.html", name="World")
-
-
 @app.route('/A/')
 def A_socialscience():
     return render_template("A.html")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
